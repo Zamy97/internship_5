f0a037ac3bc4ee0d465c6bb8aefce5bfe13e9722
Aktar Zaman <aktarzaman@berkeley.edu>
2020-11-15 21:12:41 -0500

    first commit

---
+++ internship_5/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from internship_5.users.models import User
+from internship_5.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
